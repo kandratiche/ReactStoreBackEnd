4d7afbba0869a0bcf532dc8b92cbd82a09368089
kandratiche <cgnipnoob@gmail.com>
2025-04-18 20:05:47 +0500

    BLYA

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ dotenv.config();
 const mongo_uri = process.env.MONGO_URI
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
